e3f94ffc1b00e73bf1bbe34160a5e168ef2c52ed
Nadson O. Coelho <nadsoncoelhofsa@gmail.com>
2025-06-15 15:41:50 -0300

    Estrutura-inicial

---
+++ src/components/login.jsx
@@ -0,0 +1,21 @@
+
+const Login = () => {
+  return (
+    <div className='container'>
+        <form>
+            <h1>Acesse o sistema</h1>
+            <div>
+            <input type="email" placeholder='E-mail'/>
+            </div>
+            <div>
+            <input type="password" placeholder="senha" />
+            </div>
+            <button>Entrar</button>
+
+        </form>
+      
+    </div>
+  )
+}
+
+export default Login
