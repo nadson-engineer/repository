ebaee93acc711612babb9ceeb666edd136bc86e3
Nadson O. Coelho <nadsoncoelhofsa@gmail.com>
2025-06-29 12:42:23 -0300

    Tela-login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,17 +1,48 @@
+import {FaUser, FaLock} from 'react-icons/fa';
+import { useState } from 'react';
+import "./Login.css";
+
+
 
 const Login = () => {
+
+    const [username, setusername] = useState('')
+    const [userpassword, setuserpassword] = useState('')
+
+    const handleSumit = (event) => {
+        event.preventDefault();
+        alert('Você entrou, parabéns' + username + userpassword)
+    }
+
   return (
     <div className='container'>
-        <form>
-            <h1>Acesse o sistema</h1>
-            <div>
-            <input type="email" placeholder='E-mail'/>
-            </div>
-            <div>
-            <input type="password" placeholder="senha" />
-            </div>
-            <button>Entrar</button>
 
+        <form onSubmit={handleSumit}>
+            <h1>Acesse o sistema</h1>
+            <div className='input-field'>
+            <input type="email" placeholder='E-mail' onChange={(e) => setusername(e.target.value)}/>
+            <FaUser className="icon" />
+            </div>
+            <div className='input-field'>
+            <input type="password" placeholder="senha" onChange={(e) => setuserpassword(e.target.value)} />
+            <FaLock className='icon' />
+            </div>
+            
+            <div className='recall-forget'>
+            <label>
+                <input type="checkbox"/>
+                Lembre de mim 
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+            </div>
+            
+            <button className='login'>Entrar</button>
+
+            <div className='signup-link'>
+                <p>
+                    Não tenho uma conta? <a href="#">Registrar</a>
+                </p>
+            </div>
         </form>
       
     </div>
